c1017763c14f6bbf085aa43b74b07aa9f48feadc
Bree2003 <breeale2003@gmail.com>
2024-02-28 20:18:18 -0300

    finished template for the menu's views

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -2,3 +2,9 @@
 var email = "user@gmail.com";
 var password = "User123";
 
+// form
+var myForm = document.getElementById("miFormulario");
+
+myForm.addEventListener("submit", (e) => {
+    event.preventDefault();
+});
