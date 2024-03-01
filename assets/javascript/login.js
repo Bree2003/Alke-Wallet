34544373b5120f5593b52e7176af82cb6336404a
Bree2003 <breeale2003@gmail.com>
2024-03-01 19:16:50 -0300

    Adding interactivity to login view
    Adding visual features when the credentials are wron

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -2,9 +2,26 @@
 var email = "user@gmail.com";
 var password = "User123";
 
-// form
-var myForm = document.getElementById("miFormulario");
+// login form validation
+$(document).ready(function () {
+    $("#loginForm").submit(function (event) {
+        event.preventDefault();
 
-myForm.addEventListener("submit", (e) => {
-    event.preventDefault();
+        // capture of the values
+        let emailInput = $("#email").val();
+        let passwordInput = $("#password").val();
+        
+        // login validation
+        if (emailInput === email && passwordInput === password) {
+            alert("Beign redirected to menu...");
+            window.location.href = "menu.html";
+        } else if (emailInput === "" && passwordInput === "") {
+            alert("Credentials are empty");
+        }
+            else {
+            $("#email").addClass("wrong-input");
+            $("#password").addClass("wrong-input");
+            $("#credentials").addClass("wrong-span");
+        }
+    });
 });
